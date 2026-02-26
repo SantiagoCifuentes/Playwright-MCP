ff637385058ff6041a8cd42344e0e8e760a89d37
santcial <santiago.ci9619@gmail.com>
2026-02-26 16:20:10 -0500

    workaround for timeout error in jenkins pipeline for webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,17 +20,18 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: process.env.CI ? 60000 : 30000,
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-     actionTimeout: process.env.CI ? 15000 : 5000,
+    actionTimeout: process.env.CI ? 15000 : 5000,
   },
 
   /* Configure projects for major browsers */
